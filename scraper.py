11cc5416886609e9b04d0e3626690659f1c07768
Vit Zelinka <zelinka.vit@sspbrno.cz>
2021-01-21 21:48:05 +0100

    fully working sbazar scraping

--- scraper.py
+++ scraper.py
@@ -1,13 +1,24 @@
 from bs4 import BeautifulSoup
 import requests
+import mysql.connector
+import time
 
-#get list of urls
-#check if urls already in DB
-#go through remaining urls
-#get item header
-#get item picture url
-#save all to DB
-def scrapeSbazar():
+mysql = mysql.connector.connect(
+  host="192.168.0.188",
+  user="client",
+  password="memicko",
+  database = "bazarscraper"
+)
+cur = mysql.cursor()
+cur.close()
+
+#get list of urls - OK
+#check if urls already in DB - OK
+#go through remaining urls - OK
+#get item heading - OK
+#get item picture url - OK
+#save all to DB - OK
+def scrapeNewSbazarURLs():
     url = "https://www.sbazar.cz/30-elektro-pocitace/cela-cr/zdarma?cena-dohodou=bez"
     r = requests.get(url)
     if r.status_code == 200:
@@ -17,5 +28,32 @@ def scrapeSbazar():
         for item in items:
             anchor = item.find("a", class_="c-item__link")
             urls.append(anchor["href"])
+        cur = mysql.cursor()
+        newUrls = []
+        for url in urls:
+            cur.execute(f"SELECT EXISTS(SELECT * FROM items WHERE url = '{url}');")
+            dbresponse = cur.fetchall()
+            if dbresponse[0][0] == 0:
+                newUrls.append(url)
+        cur.close()
+        return newUrls
 
-scrapeSbazar()
+
+def scrapeSbazarData(urls):
+    cur = mysql.cursor()
+    for url in urls:
+        r = requests.get(url)
+        if r.status_code == 200:
+            soup = BeautifulSoup(r.text, "html.parser")
+            img = soup.find("img", class_="ob-c-gallery__img")
+            try:
+                imgURL = img["src"]
+            except:
+                imgURL = "placeholder.png"
+            heading = soup.find("h1", class_="p-uw-item__header").text
+            cur.execute(f"INSERT INTO items (url, imgurl, heading, bazar) VALUES ('{url}', '{imgURL}', '{heading}', 'sbazar');")
+            mysql.commit()
+    cur.close()
+    
+
+scrapeSbazarData(scrapeNewSbazarURLs())
